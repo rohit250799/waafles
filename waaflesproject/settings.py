aab077f180ea195d7b675b6468f177ff97a82f79
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-02-23 08:40:47 +0530

    Specifying the media url and the media root for the project.

--- waaflesproject/settings.py
+++ waaflesproject/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+#configuring the settings
+
 
 # Application definition
 
@@ -121,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
