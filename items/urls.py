c0f1fa4a45b3e19c8c35128890582b3b1e3945f6
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-02-27 07:40:09 +0530

    Adding url path for creating the new item

--- items/urls.py
+++ items/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+#import views
+
+app_name = 'items'
 
 urlpatterns = [
-    path('items/', views.itemsTest, name='items'),
+    #path('items/', views.itemsTest, name='items'),
+    path('', views.itemsTest, name='items'),
+    path('new/', views.newitem, name='newitem'),
 ]
